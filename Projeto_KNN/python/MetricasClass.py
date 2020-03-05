cd8cd175892e7480bd0968475ed150fd106a011a
Dimitri Leandro <dimitri.leandro@gmail.com>
2020-03-05 20:11:16 -0300

    Resultados e gráficos gerados, agr é só escrever o relatório

--- Projeto_KNN/python/MetricasClass.py
+++ Projeto_KNN/python/MetricasClass.py
@@ -1,49 +1,64 @@
 import numpy as np
+import math
 
 class Metricas:
 
-	acuracia        = 0
-	precisoes  = [] 
-	revocacoes = []
+	acuracia          = 0
+	precisoes         = [] 
+	revocacoes        = []
+	mediaPrecisoes    = 0
+	mediaRevocacoes   = 0
+	desvPadPrecisoes  = 0
+	desvPadRevocacoes = 0
 
 	def __init__(self, matrizConfusao):
-		self.acuracia        = self.calcularAcuracia(matrizConfusao)
-		self.precisoes  = self.calcularPrecisoes(matrizConfusao)
-		self.revocacoes = self.calcularRevocacoes(matrizConfusao)
+		self.acuracia          = self.calcularAcuracia(matrizConfusao)
+		self.precisoes         = self.calcularPrecisoes(matrizConfusao)
+		self.revocacoes        = self.calcularRevocacoes(matrizConfusao)
+		self.mediaPrecisoes    = np.mean(self.precisoes)
+		self.mediaRevocacoes   = np.mean(self.revocacoes)
+		self.desvPadPrecisoes  = np.std(self.precisoes)
+		self.desvPadRevocacoes = np.std(self.revocacoes)
 
 	def calcularAcuracia(self, matrizConfusao):
 		# DEFINIDA COMO (TP + TN)/(TP + FP + TN + FN) -> VALORES DA DIAGONAL PRINCIPAL SOMADOS DIVIDIDO PELO VALOR DE TODAS AS CELULAS SOMADAS
-		somaDiagonalPrincipal = 0
-		somaTudo              = 0
-
-		for i in range(len(matrizConfusao)):
-			for j in range(len(matrizConfusao[i])):
-				
-				somaTudo += matrizConfusao[i][j]
-				
-				if i == j:
-					somaDiagonalPrincipal += matrizConfusao[i][j]
+		somaDiagonalPrincipal = np.sum(np.diag(matrizConfusao))
+		somaTudo              = np.sum(matrizConfusao)
 
 		return somaDiagonalPrincipal / somaTudo
 
-	def calcularPrecisoes(self, matrizConfusao):
-		
-		arrayPrecisoes = []
-
-		for i, linha in enumerate(matrizConfusao):
-			precisaoAtual = linha[i]/sum(linha)
-			arrayPrecisoes.append(precisaoAtual)
-
-		return arrayPrecisoes
-
 	def calcularRevocacoes(self, matrizConfusao):
 		
-		transposta = np.array(matrizConfusao).T
-
 		arrayRevocacoes = []
 
-		for i, linha in enumerate(transposta):
-			revocacaoAtual = linha[i]/sum(linha)
+		for i, linha in enumerate(matrizConfusao):
+			
+			somaLinha = np.sum(linha)
+
+			if somaLinha != 0: 
+				revocacaoAtual = linha[i]/somaLinha
+			else:
+				revocacaoAtual = 0
+
 			arrayRevocacoes.append(revocacaoAtual)
 
 		return arrayRevocacoes
+
+	def calcularPrecisoes(self, matrizConfusao):
+		
+		transposta = np.array(matrizConfusao).T
+
+		arrayPrecisoes = []
+
+		for i, linha in enumerate(transposta):
+			
+			somaLinha = np.sum(linha)
+
+			if somaLinha != 0: 
+				precisaoAtual = linha[i]/somaLinha
+			else:
+				precisaoAtual = 0
+
+			arrayPrecisoes.append(precisaoAtual)
+
+		return arrayPrecisoes
